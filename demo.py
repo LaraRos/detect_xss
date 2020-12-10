b5527d59a076a878258cfefed509db03449adfdf
larasm <larasm@kth.se>
2020-12-10 15:18:48 +0100

    Add examples and add graph examples

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
 import heapq
 import sys
-
+import matplotlib.pyplot as plt
 
 class TrainingPoint:
 	def __init__(self, string, c = -1):
-		self.string = string
+		self.string = string.lower()
 		self.x = self.calc1()
 		self.y = self.calc2()
 		self.c = c
@@ -24,31 +24,25 @@ class TrainingPoint:
 			res += self.string.__contains__(keyword)
 		return res
 
-class TrainingPoints:
-	def __init__(self):
-		self.points = []
-
-	def add_point(self, point):
-		self.points.append(point)
-
 def distance(point1, point2):
 	return (point1.x - point2.x)**2 + (point1.y - point2.y)**2
 
 def train_points():
-	training_points = TrainingPoints()
+	training_points = []
 	with open('training.txt', 'r') as file:
 		num_points = int(file.readline())
 		for i in range(num_points):
 			string = file.readline()
 			c = int(file.readline())
 			training_point = TrainingPoint(string, c)
-			training_points.add_point(training_point)
+			# training_points.add_point(training_point)
+			training_points.append(training_point)
 	return training_points
 
 def find_k_nearest_neighbour(k, point, points):
 	best_neighbours = []
 	heapq.heapify(best_neighbours)
-	for neighbour in points.points:
+	for neighbour in points:
 		d = distance(point, neighbour)
 		c = neighbour.c
 		heapq.heappush(best_neighbours, (d,c))
@@ -60,28 +54,34 @@ def find_k_nearest_neighbour(k, point, points):
 	return malicious > k//2
 
 def main():
-	k = 3
+	k = 1
 	training_points = train_points()
+	x_malicious = []
+	x_benign = []
+	y_malicious = []
+	y_benign = []
+	for point in training_points:
+		# point is malicious
+		if point.c:
+			x_malicious.append(point.x)
+			y_malicious.append(point.y)
+		# point is benign
+		else:
+			x_benign.append(point.x)
+			y_benign.append(point.y)
+	plt.show()
 	while True:
 		string = input("Enter your string: ")
+		if string == 'x':
+			break
 		point = TrainingPoint(string)
 		print(find_k_nearest_neighbour(k, point, training_points))
+		plt.scatter(x_malicious,y_malicious, color = 'red', label = 'malicious')
+		plt.scatter(x_benign,y_benign, color = 'blue', label ='benign')
+		plt.scatter(point.x, point.y, color = 'black')
+		plt.xlabel('Measure of readability')
+		plt.ylabel('Number of HTML keywords') 
+		plt.legend()
+		plt.show()
 
 main()
-
-
-
-
-
-
-		
-
-
-
-
-
-
-
-		
-
-
